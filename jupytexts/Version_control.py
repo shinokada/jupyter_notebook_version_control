01878a6885215ff6445ed04a5764e386388fc118
Shinichi Okada <okada.shin@gmail.com>
2020-02-18 20:25:52 +0900

    Added gitignore

---
+++ jupytexts/Version_control.py
@@ -0,0 +1,22 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+x = np.arange(-3, 3, 0.1)
+y = np.cos(x)
+plt.plot(x, y)
+plt.show()
+
+# !jupytext --to py --output ./jupytexts/Version_control.py ./Version_control.ipynb
+
+
